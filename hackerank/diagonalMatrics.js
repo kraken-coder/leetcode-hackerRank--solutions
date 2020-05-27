3625e8c21209da41d0e8308ef96a6f760a0b6b93
Siradji Awoual <siradjiawoual@gmail.com>
2020-05-27 08:09:51 +0000

    Diagonal Matrics

---
+++ hackerank/diagonalMatrics.js
@@ -0,0 +1,23 @@
+function diagonalDifference(arr) {  
+    let leftDiag = 0
+    let rightDiag = 0
+    let length = arr.length
+    for (var i = 0, j = length - 1; i < length; i++, j--) {
+            leftDiag += arr[i][i]
+            rightDiag += arr[i][j]  
+            // console.log( 'j', arr[i][i])
+            // console.log( '1', arr[i][j])
+
+            // console.log( 'i', i)
+    }
+
+    console.log(leftDiag)
+    console.log(rightDiag)
+    return Math.abs(leftDiag - rightDiag)
+
+}
+
+
+
+
+console.log(diagonalDifference([ [1,2,4], [3,5,5], [1,5,-4]]))
