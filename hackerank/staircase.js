6f009fd30a054c15000024c499b9d59f917bb01f
Siradji Awoual <siradjiawoual@gmail.com>
2020-05-27 09:18:27 +0000

    staircase challenge

---
+++ hackerank/staircase.js
@@ -0,0 +1,18 @@
+
+
+
+const stairCase = (num) => {
+    const consoleGen  = (x , v) => {
+        const hash = '#'
+        const space = ' '
+        console.log( i.repeat(space - 1)  +  s.repeat( hash + 1) )
+    }
+    for (let i = 0,  v = num;  i < num; i++,  v--) {
+        consoleGen(i, Math.abs(v))
+    }
+    return 
+}
+
+
+
+stairCase(10)
